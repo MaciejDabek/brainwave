a0bf9826c01c919f7edb60f235da13f4af6d851b
MaciejDabek <mdabekhub@gmail.com>
2024-09-08 17:48:56 +0200

    add roadmap

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Collaboration />
         <Services />
         <Pricing />
+        <Roadmap />
       </div>
       <ButtonGradient />
     </BrowserRouter>
